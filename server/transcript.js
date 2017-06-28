2ba75ecda5ee0744d42b1b5398f7f35a19dfbab6
Zach Caceres <zachcaceres@gmail.com>
2017-06-28 16:43:24 -0400

    feat: add and test raw file streaming from POST request to temp folder

--- server/transcript.js
+++ server/transcript.js
@@ -1,12 +1,20 @@
 'use strict'
-
+const fs = require('fs')
 const transcript = require('express').Router()
+const TRANSCRIPTION_ID = 1
 
-transcript.get('/', (req, res, next) => {
+transcript.get('/:id', (req, res, next) => {
+  // TODO: find model by ID, use ID to name file for transcription
   res.sendStatus(200)
 })
 
-// TODO: GET route here
-// TODO: POST route here
+transcript.post('/', (req, res, next) => {
+  // TODO: check for multipart headers, use multer to stream
+
+  // Direct file stream
+  let writeStream = fs.createWriteStream(`audio-to-transcribe/${TRANSCRIPTION_ID}.mp3`) // set name based on ID
+  req.pipe(writeStream)
+  res.sendStatus(200)
+})
 
 module.exports = transcript
