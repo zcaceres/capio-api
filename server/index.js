25f9e5017a8d77753d9130bfe9eca15921e6f1a1
Zach Caceres <zachcaceres@gmail.com>
2017-06-28 13:53:17 -0400

    feat: add centralized error handling middleware for all routes other than /transcript

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 const app = require('express')()
 const PORT_NUM = 8080
 
+// Router for Transcript service
 app.use('/transcript', require('./transcript'))
+
+// Route to handle requests to anywhere else
+app.get('*', function(req, res, next) {
+  let err = new Error('Page Not Found!')
+  err.statusCode = 404
+  next(err)
+})
+
+// TODO: Add middleware here
+
+/* Error Handling Middleware */
+app.use(function(err, req, res, next) {
+  console.error(err.message)
+  if (!err.statusCode) err.statusCode = 500 // Sets generic server error status code if none on 'err'
+  res.status(err.statusCode).send(err.message)
+})
 
 app.listen(PORT_NUM, () => {
   console.log(`---Started HTTP Server for Capio Transcription Service---`)
   console.log(`Listening on http://localhost:${PORT_NUM}`)
 })
+
+module.exports = app
--- server/transcript.js
+++ server/transcript.js
@@ -2,6 +2,10 @@
 
 const transcript = require('express').Router()
 
+transcript.get('/', (req, res, next) => {
+  res.sendStatus(200)
+})
+
 // TODO: GET route here
 // TODO: POST route here
 
