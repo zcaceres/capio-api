6d465d283a06dc57fc96649f1d1ee5163d5f59c6
Zach Caceres <zachcaceres@gmail.com>
2017-06-28 14:16:01 -0400

    fix: use app.all to make sure error handler works for all HTTP verbs

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
 const app = require('express')()
-const PORT_NUM = 8080
+const PORT_NUMBER = 8080
 
-// Router for Transcript service
+/* Router for Transcript service */
 app.use('/transcript', require('./transcript'))
 
-// Route to handle requests to anywhere else
-app.get('*', function(req, res, next) {
-  let err = new Error('Page Not Found!')
+/* Handles requests to anywhere other than /transcript */
+app.all('*', function(req, res, next) {
+  let err = new Error('Nothing found at ' + req.originalUrl)
   err.statusCode = 404
   next(err)
 })
 
 // TODO: Add middleware here
 
-/* Error Handling Middleware */
+/* Error Handling */
 app.use(function(err, req, res, next) {
-  console.error(err.message)
+  console.error('ERROR:', err.message)
   if (!err.statusCode) err.statusCode = 500 // Sets generic server error status code if none on 'err'
   res.status(err.statusCode).send(err.message)
 })
 
-app.listen(PORT_NUM, () => {
+app.listen(PORT_NUMBER, () => {
   console.log(`---Started HTTP Server for Capio Transcription Service---`)
-  console.log(`Listening on http://localhost:${PORT_NUM}`)
+  console.log(`Listening on http://localhost:${PORT_NUMBER}`)
 })
 
 module.exports = app
